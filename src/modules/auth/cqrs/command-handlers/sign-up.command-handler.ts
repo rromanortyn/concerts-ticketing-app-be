f49af265c3dadbd92b8b2323101567b4f2e5a643
unknown <rromanortyn@gmail.com>
2026-01-04 14:14:33 +0200

    Fixed email uniqueness check in sign up

--- src/modules/auth/cqrs/command-handlers/sign-up.command-handler.ts
+++ src/modules/auth/cqrs/command-handlers/sign-up.command-handler.ts
@@ -5,7 +5,7 @@ import SignUpCommandOutput from '../../types/classes/command-outputs/sign-up.com
 import AccessTokenService from '../../services/access-token.service'
 import { InjectRepository } from '@nestjs/typeorm'
 import UserEntity from 'src/data/entities/user.entity'
-import { Repository } from 'typeorm'
+import { QueryFailedError, Repository, TypeORMError } from 'typeorm'
 import { ConflictException } from '@nestjs/common'
 import PasswordService from '../../services/password.service'
 
@@ -21,32 +21,29 @@ class SignUpCommandHandler implements ICommandHandler<SignUpCommand> {
   async execute(command: SignUpCommand): Promise<SignUpCommandOutput> {
     const { email, password } = command.input
 
-    const userByEmailExists = await this.userRepository.exists({
-      where: {
-        email,
-      },
-      select: {
-        id: true,
-      },
-    })
+    const hashedPassword = await this.passwordService.hashPassword(password)
 
-    if (userByEmailExists) {
-      throw new ConflictException({
-        code: 'USER_ALREADY_EXISTS',
-        message: 'A user with the specified email already exists.',
+    let accessToken: string = ''
+
+    try {
+      const newUser = await this.userRepository.save({
+        ...command.input,
+        password: hashedPassword,
+      })
+
+      accessToken = await this.accessTokenService.sign({
+        id: newUser.id,
       })
     }
 
-    const hashedPassword = await this.passwordService.hashPassword(password)
-
-    const newUser = await this.userRepository.save({
-      ...command.input,
-      password: hashedPassword,
-    })
-
-    const accessToken = await this.accessTokenService.sign({
-      id: newUser.id,
-    })
+    catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException({
+          code: 'USER_ALREADY_EXISTS',
+          message: 'A user with the specified email already exists.',
+        })
+      }
+    }
 
     return {
       data: {
